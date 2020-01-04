8f1c63f080cedf16bd23fbc8467e3a7706d95ed3
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-04 08:18:56 +0700

    commit

--- php/db.php
+++ php/db.php
@@ -1,3 +1,3 @@
 <?php
-$c = new mysqli("localhost", "news", "HelloWorld@123");
-$c->select_db("news");
+$c = new mysqli("localhost", "osgk3177_news", "HelloWorld@123");
+$c->select_db("osgk3177_news");
